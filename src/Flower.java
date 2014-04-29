15ae87d1c93191588f815e856be398a9baa339df
Lera <Valery790@gmail.com>
2014-04-30 01:05:56 +0300

    Update Flower.java
    change file Flower.java

--- src/Flower.java
+++ src/Flower.java
@@ -2,17 +2,29 @@ import java.util.Vector;
 
 public class Flower extends Plant, Plant implements Goods {
 
-  private Integer price;
+  protected Integer price;
 
     public Vector  myFlowerSet;
-      public Vector  myPlant;
+    public Vector  myPlant;
     public Vector  myFlowerApplication;
-    public Vector  myFlowerApplication;
-    public Vector  myFlowerSet;
-    public Vector  myFlowerSet;
-    public Vector  myFlowerSet;
 
   public void Flower( name,  price) {
   }
+
+  public void setPrice(Integer price) {
+        if (price >= 0)
+            this.price = price;
+        else
+            throw new IllegalArgumentException("Not supported negative value");
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public Flower(String name, Integer price){
+        setName(name);
+        setPrice(price);
+    }
 
 }
